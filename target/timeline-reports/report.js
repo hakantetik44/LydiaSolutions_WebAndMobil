32923c5ca5e6d780f7e8432c4a13fccec439f952
Hakan Tetik <fildisili88@gmail.com>
2025-02-28 22:44:12 +0100

    Reviewed project structure and addressed CukesRunner issue

--- target/timeline-reports/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"cf95b9fc-595e-4181-93c8-1ba18ffb4695","feature":"Navigation vers la page d'accueil de l'application Wigl","scenario":"Accès à la page d'accueil et sélection de la langue française","start":1740774136982,"group":1,"content":"","tags":"@wigl,@accueil,@smoke,@homepage,@language,","end":1740774149582,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
-});
